92e2b7beadbdbadd894b3a4b43e02a3bf5b0aa15
Evgeny Kuptsov <kupzov200@mail.ru>
2026-04-30 16:32:48 +0400

    chore: installation and configuration of project configs

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app';
+
+const root = document.querySelector('#root');
+
+if (!root) {
+  throw new Error('root element is not defined');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
